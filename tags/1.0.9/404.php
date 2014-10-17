f38a0ef3e75991a70f84f40fefc9e7f0321d4fd2
Marius Cristea <cristea.marius.alexandru@gmail.com>
2014-10-17 17:05:03 -0500

    Sync with developemnt

--- tags/1.0.9/404.php
+++
@@ -1,9 +0,0 @@
-<?php get_header(); ?>
-<div id="error"></div>
-<div id="searcherror">
-<!--Search-->
-<div class="notfound">	<?php get_search_form(); ?>
-</div>
-<!--Search-->
-</div>
-<?php get_footer(); ?>
